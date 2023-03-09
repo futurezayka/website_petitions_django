13753e951f65925368a62beff868662382c5973e
futurefkslaves <kurach.vladyslav@lll.kpi.ua>
2023-03-09 20:15:22 +0200

    added json parser for voting status

--- peticions/urls.py
+++ peticions/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('create/', views.create, name = "create"),
     path('<int:pk>', views.PeticionsDetailView.as_view(), name = 'detail'),
     path('<int:pk>/edit', views.PeticionUpdateView.as_view(), name='edit'),
-    path('<int:id>/delete-item', views.deleteItem,name='delete-item'),
-    path('<int:id>/vote', views.vote_petition,name='vote')
+    path('<int:id>/delete-item', views.deleteItem, name='delete-item'),
+    path('<int:id>/vote', views.vote_petition, name='vote')
 ]
